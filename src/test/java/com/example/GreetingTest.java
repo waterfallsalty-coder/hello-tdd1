f90776dec65a565f4734e65a6a6ecdd0cf3527fb
Your Name <waterfallsalty@gmail.com>
2025-10-31 10:43:42 +0700

    pass test

--- src/test/java/com/example/GreetingTest.java
+++ src/test/java/com/example/GreetingTest.java
@@ -1,17 +1,13 @@
-Java
+package com.example;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class GreetingTest {
     @Test
     void testHelloBasic() {
-// 1. Arrange (ยงัไมม่ คี ลาสนี้)
-// Greeting greeting = new Greeting();
-// 2. Act (ยงัไมม่ เีมธอดนี้)
-// String result = greeting.sayHello("World");
-// 3. Assert
-// assertEquals("Hello, World", result);
-// ตอนนี้เราจะ "แกล้ง" ให้มนั พงั
-        assertEquals("Hello, World", "test failed");
+        Greeting greeting = new Greeting();
+        String result = greeting.sayHello("World");
+        assertEquals("Hello, World", result);
     }
 }
-
